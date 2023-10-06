64328e3f3eb33e2f1cd5f0b365d7894187444522
c0mpile <c0mpile@disroot.org>
2023-10-06 19:52:22 -0400

    breaking stuff

---
+++ test/chroot.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+unset installed_gpu,locale_num,profile_num,kb_map
+
+# updating environment
+env-update
+source /etc/profile
+PS1="(chroot) ${PS1}"
+
+# sync portage snapshot
+echo "--- Syncing portage snapshot ---"
+emerge-webrsync &
+wait
+
+# setting locale
+echo "--- Setting locale ---"
+sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
+(locale-gen &)
+wait
+echo "LANG=\"en_US.UTF-8\"
+LC_COLLATE=\"C.UTF-8\"" > /etc/env.d/02locale
+localectl set-locale LANG=en_US.UTF-8
+source /etc/profile
+env-update
+PS1="(chroot) ${PS1}"
+
+# set keyboard layout
+echo "--- Setting keyboard layout ---"
+read -p "Enter desired keyboard layout: " kb_map
+localectl set-keymap ${kb_map}
+localectl set-x11-keymap ${kb_map}
+
+# list and select profile
+eselect profile list
+read -p "Enter the number of your desired profile: " profile_num
+eselect profile set ${profile_num}
+
+# emerging necessary packages for setup
+echo "--- Emerging necessary packages for setup ---"
+emerge --ask --quiet-build --autounmask-write app-portage/cpuid2cpuflags sys-apps/lshw app-eselect/eselect-repository dev-vcs/git
+dispatch-conf
+emerge --ask --quiet-build app-portage/cpuid2cpuflags sys-apps/lshw app-eselect/eselect-repository dev-vcs/git
+
+# detecting gpu vendor
+echo "--- Detecting GPU --- "
+if [[ $(lshw -C display | grep vendor) =~ Nvidia ]]; then
+	export installed_gpu="nvidia"
+elif [[ $(lshw -C display | grep vendor) =~ AMD ]]; then
+	export installed_gpu="amdgpu radeonsi"
+elif [[ $(lshw -C display | grep vendor) =~ Intel ]]; then
+	export installed_gpu="intel"
+fi
+
+# writing make.conf
+echo "--- Writing make.conf --- "
+echo "COMMON_FLAGS=\"-march=native -O2 -pipe\"
+CFLAGS=\"\${COMMON_FLAGS}\"
+CXXFLAGS=\"\${COMMON_FLAGS}\"
+FCFLAGS=\"\${COMMON_FLAGS}\"
+FFLAGS=\"\${COMMON_FLAGS}\"
+LDFLAGS=\"-Wl,-O2 -Wl,--as-needed\"
+$(cpuid2cpuflags | sed 's/:\s/="/; s/$/"/')
+RUST_FLAGS=\"-C debuginfo=0 -C codegen-units=1 -C target-cpu=native -C opt-level=2\"
+MAKEOPTS=\"-j$(nproc) -l$(nproc)\"
+EMERGE_DEFAULT_OPTS=\"--jobs $(nproc) --load-average $(nproc) --keep-going --verbose --quiet-build --with-bdeps=y --complete-graph=y --deep --ask\"
+
+USE=\"minimal X wayland xwayland screencast systemd btrfs pipewire pulseaudio alsa sndfile taglib gstreamer opengl x264 x265 v4l vdpau vaapi lv2 vulkan gtk qt5 qt6 udisks udev dbus policykit networkmanager ipv6 nftables native-symlinks lto pgo jit xs orc threads asm openmp zstd lz4 ffmpeg icu av1 harfbuzz jpeg libevent librnp libvpx png webp ssl zlib cups usb offensive -dvd -cdr -ios -ipod -clamav -gnome -kde -selinux -ufw -iptables -qtwebengine -webengine\"
+
+ACCEPT_KEYWORDS=\"~amd64\"
+ACCEPT_LICENSE=\"*\"
+
+DISTDIR=\"/var/cache/distfiles\"
+PKGDIR=\"/var/cache/binpkgs\"
+LC_MESSAGES=C.utf8
+
+VIDEO_CARDS=\"${installed_gpu}\"
+INPUT_DEVICES=\"libinput evdev joystick\"
+GRUB_PLATFORMS=\"efi-64\"
+
+GENTOO_MIRRORS=\"https://mirrors.mit.edu/gentoo-distfiles https://mirrors.rit.edu/gentoo https://mirror.leaseweb.com/gentoo https://gentoo.osuosl.org https://mirror.clarkson.edu/gentoo\"
+FEATURES=\"candy fixlafiles parallel-install parallel-fetch\"
+FETCHCOMMANDWRAPPER_OPTIONS=\"--max-concurrent-downloads=50 --max-connection-per-server=50 --min-split-size=10M --max-file-not-found=1 --max-tries=1\"" > /etc/portage/make.conf
+
+# set package.use
+echo "--- Writing package.use --- "
+rm -rf /etc/portage/package.use
+cp /root/gentoo-setup/portage/package.use /etc/portage
+
+# emerging profile
+echo "--- Emerging @world...this could take awhile ---"
+USE="-gpm" emerge --oneshot --ask ncurses
+emerge --oneshot --ask gpm
+emerge --oneshot --ask sys-apps/portage
+emerge --oneshot --ask ncurses
+emerge -vuDN @world
+
+# copy custom kernel config snippets
+echo "--- Copying custom kernel config snippets ---"
+mkdir -p /etc/kernel/config.d/
+cp /root/gentoo-setup/kernel/* /etc/kernel/config.d/
+
+# emerge kernel
+emerge --ask sys-kernel/linux-firmware
+emerge --ask sys-kernel/installkernel-gentoo
+emerge --ask sys-kernel/gentoo-kernel
---
+++ test/install.sh
@@ -0,0 +1,154 @@
+#!/usr/bin/env bash
+
+# unset variables
+unset disk,target_part1,target_part2,subvol_options,current_stage3
+
+# target installation drive
+read -p "Enter the installation target device including /dev/: " disk
+
+if [[ $(echo $disk | grep dev) =~ nvme ]]; then
+        export target_part1="${disk}p1"
+        export target_part2="${disk}p2"
+elif [[ $(echo $disk | grep dev) =~ sd ]]; then
+        export target_part1="${disk}1"
+        export target_part2="${disk}2"
+elif [[ $(echo $disk | grep dev) =~ hd ]]; then
+        export target_part1="${disk}1"
+        export target_part2="${disk}2"
+elif [[ $(echo $disk | grep dev) =~ vd ]]; then
+        export target_part1="${disk}1"
+        export target_part2="${disk}2"
+fi
+
+# updating pacman keyring
+echo "--- Updating pacman keyring ---"
+(pacman --noconfirm -Sy archlinux-keyring &)
+wait
+pacman-key --init
+pacman-key --populate archlinux
+
+# install git
+echo "--- Installing git ---"
+(pacman --noconfirm -Sy git glibc &)
+wait
+# wipe target drive
+echo "--- Wiping target drive --- "
+wipefs -af $disk
+sgdisk --zap-all --clear $disk
+partprobe $disk
+
+# create partitions
+echo "--- Creating partitions --- "
+sgdisk -n 0:0:+512MiB -t 0:ef00 -c 0:esp $disk
+sgdisk -n 0:0:0 -t 0:8309 -c 0:luks $disk
+partprobe $disk
+
+# encrypt and unlock root device
+echo "--- Setting up encryption --- "
+cryptsetup --type luks1 -v -y luksFormat ${target_part2}
+cryptsetup luksOpen ${target_part2} crypt
+
+# create efi boot partition and btrfs root
+echo "--- Formatting partitions --- "
+mkfs.vfat -F32 -n ESP ${target_part1}
+mkfs.btrfs -L gentoo /dev/mapper/crypt
+
+# create chroot directory and mount encrypted btrfs partition
+echo "--- Mounting encrypted partition --- "
+mkdir /mnt/gentoo
+mount /dev/mapper/crypt /mnt/gentoo
+
+# create btrfs subvolumes
+echo "--- Creating BTRFS subvolumes --- "
+btrfs su cr /mnt/gentoo/@
+btrfs su cr /mnt/gentoo/@home
+btrfs su cr /mnt/gentoo/@opt
+btrfs su cr /mnt/gentoo/@srv
+btrfs su cr /mnt/gentoo/@snapshots
+btrfs su cr /mnt/gentoo/@log
+btrfs su cr /mnt/gentoo/@tmp
+btrfs su cr /mnt/gentoo/@cache
+btrfs su cr /mnt/gentoo/@images
+
+# unmount chroot directory
+umount /mnt/gentoo
+
+# set mount options for subvolumes
+echo "--- Setting subvolume mount options --- "
+subvol_options="rw,noatime,compress=zstd,space_cache=v2"
+
+# mount root filesystem
+echo "--- Mounting root filesystem --- "
+mount -o ${subvol_options},subvol=@ /dev/mapper/crypt /mnt/gentoo/
+
+# create mountpoints for subvolumes
+echo "--- Creating mountpoints --- "
+mkdir -p /mnt/gentoo/{home,opt,srv,.snapshots,var/log,var/tmp,var/cache,var/lib/libvirt/images,boot/efi}
+
+# mount remaining subvolumes
+echo "--- Mounting remaining subvolumes --- "
+mount -o ${subvol_options},subvol=@home /dev/mapper/crypt /mnt/gentoo/home
+mount -o ${subvol_options},subvol=@opt /dev/mapper/crypt /mnt/gentoo/opt
+mount -o ${subvol_options},subvol=@srv /dev/mapper/crypt /mnt/gentoo/srv
+mount -o ${subvol_options},subvol=@snapshots /dev/mapper/crypt /mnt/gentoo/.snapshots
+mount -o ${subvol_options},subvol=@log /dev/mapper/crypt /mnt/gentoo/var/log
+mount -o ${subvol_options},subvol=@tmp /dev/mapper/crypt /mnt/gentoo/var/tmp
+mount -o ${subvol_options},subvol=@cache /dev/mapper/crypt /mnt/gentoo/var/cache
+mount -o ${subvol_options},subvol=@images /dev/mapper/crypt /mnt/gentoo/var/lib/libvirt/images
+
+# mount efi boot partiton
+echo "--- Mounting EFI partition --- "
+mount ${target_part1} /mnt/gentoo/boot/efi
+
+# cloning repo to chroot
+echo "--- Cloning git repo ---"
+(git clone https://github.com/c0mpile/gentoo-setup.git /mnt/gentoo/root/gentoo-setup &)
+wait
+
+cd /mnt/gentoo
+
+# download latest stage3 tarball with systemd and merged usr
+echo "--- Downloading latest stage3 tarball with systemd and merged usr --- "
+current_stage3="$(grep -Po "^[0-9]{8}T[0-9]{6}Z/[^[:space:]]+" < <(curl --fail --show-error --location "https://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64-systemd-mergedusr.txt"))"
+curl --fail --show-error --location --remote-name-all "https://distfiles.gentoo.org/releases/amd64/autobuilds/${current_stage3}"
+
+# extract stage3 tarball
+echo "--- Extracting stage3 tarball --- "
+tar -xpf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
+
+# remove stage3 tarball
+echo "--- Removing stage3 tarball --- "
+rm -rf /mnt/gentoo/stage3-*.tar.xz
+
+# get UUIDs of target partitions
+echo "--- Finding UUIDs of target partitions --- "
+export part2_uuid=$(blkid -s UUID -o value ${target_part2})
+export mapped_uuid=$(blkid -s UUID -o value /dev/mapper/crypt)
+
+# configure dracut with correct UUIDs for initramfs generation
+echo "--- Writing dracut config --- "
+echo "add_dracutmodules+=\" crypt dm rootfs-block \"" > /mnt/gentoo/etc/dracut.conf
+echo "kernel_cmdline+=\" root=UUID=${mapped_uuid} rd.luks.uuid=${part2_uuid} \"" >> /mnt/gentoo/etc/dracut.conf
+
+# copy dns settings
+echo "--- Copying DNS settings --- "
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+
+# mount remaining necessary filesystems
+echo "--- Mounting necessary filesystems for chroot --- "
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+mount --bind /run /mnt/gentoo/run
+mount --make-slave /mnt/gentoo/run
+
+# create fstab
+echo "--- Generating fstab --- "
+genfstab -U -p /mnt/gentoo >> /mnt/gentoo/etc/fstab
+
+# enter chroot
+echo "Entering chroot..."
+#chroot /mnt/gentoo /root/gentoo-setup/chroot.sh
+chroot /mnt/gentoo /bin/bash
